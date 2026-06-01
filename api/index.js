999e91cdccbf9404cd44752f80389d0fa596fbb9
JCPrime01 <vanzokiller@gmail.com>
2026-06-01 09:56:20 -0300

    chore: shareCode T18A8Q9D7VR -> WUFV6WPZUFR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=T18A8Q9D7VR&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=WUFV6WPZUFR&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
