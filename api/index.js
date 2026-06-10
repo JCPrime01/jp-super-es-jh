cedf21ecfb39c9625541e22811e69c458ad73d97
JCPrime01 <vanzokiller@gmail.com>
2026-06-10 13:58:35 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=9G28HRPSEUJ&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7177&shareCode=9G28HRPSEUJ&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
